436f21b221b6f2bfeacbbf61a158bbcd3b93cd0c
FourSqweez <four7518@gmail.com>
2022-12-27 17:43:33 +0700

    try to created page inside app folder

---
+++ app/moon/(play)/Play.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Play = () => {
+  return <div>Play Page</div>
+}
+
+export default Play
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { notFound } from 'next/navigation'
+
+const Page = () => {
+  return <div>This is main page</div>
+}
+
+export default Page
